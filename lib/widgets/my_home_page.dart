ae1520bbd3a1166a69d27b496129dc8ae280a6f1
Daniel Stüwe <daniel.stuewe@maibornwolff.de>
2024-06-22 15:10:18 +0200

    redesign todo page

--- lib/widgets/my_home_page.dart
+++ lib/widgets/my_home_page.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/models/ModelProvider.dart';
 import 'package:myapp/todo_page.dart';
@@ -12,7 +16,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Todo> todos = [
+  List<Todo> _todos = [
     Todo(
         name: 'test',
         description: 'Ist es ein Raum? Ist es ein Berg?',
@@ -20,17 +24,43 @@ class _MyHomePageState extends State<MyHomePage> {
     Todo(
         name: 'Hallo',
         description: 'Ist es ein Raum? Ist es ein Berg?',
-        state: TodoState.NotStarted)
+        state: TodoState.NotStarted,
+        deadline: TemporalDateTime.fromString('2024-06-22T17:00Z'))
   ];
 
   void _incrementCounter() {
     setState(() {
-      todos.add(Todo(
+      _todos.add(Todo(
         name: 'New task',
         description: 'Beschreibung des neuen Raums',
         state: TodoState.NotStarted,
       ));
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshTodos();
+  }
+
+  Future<void> _refreshTodos() async {
+    try {
+      final request = ModelQueries.list(Todo.classType);
+      final response = await Amplify.API.query(request: request).response;
+
+      final todos = response.data?.items;
+      if (response.hasErrors) {
+        safePrint('errors: ${response.errors}');
+        return;
+      }
+      setState(() {
+        //_todos = todos!.whereType<Todo>().toList();
+        safePrint(_todos);
+      });
+    } on ApiException catch (e) {
+      safePrint('Query failed: $e');
+    }
   }
 
   @override
@@ -42,20 +72,36 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: ListView(
-          children: todos.map((todo) {
+          children: _todos.map((todo) {
+            final leadingIcon = switch (todo.state) {
+              TodoState.InProgress => Icon(Icons.change_circle),
+              TodoState.NotStarted => Icon(Icons.circle_outlined),
+              _ => Icon(Icons.check_circle_outline)
+            };
+            const Icon(Icons.check_box);
             return ListTile(
               title: Text(todo.name),
-              leading: const Icon(Icons.check_box),
-              trailing: const Icon(Icons.delete),
-              subtitle: Text(todo.description?.substring(0, todo.description.length < 100 ? todo.description.length : 100)),
-          
-
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        TodoPage(key: ValueKey(todo.hashCode), todo: todo)),
+              leading: IconButton(
+                icon: leadingIcon,
+                onPressed: () => {
+                  setState(() {
+                    _todos.add(todo.copyWith(state: TodoState.InProgress));
+                  })
+                },
               ),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () => {},
+              ),
+              subtitle: todo.description != null
+                  ? Text(truncate(todo.description!))
+                  : null,
+              // onTap: () => Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //       builder: (context) =>
+              //           TodoPage(key: ValueKey(todo.hashCode), todo: todo)),
+              // ),
             );
           }).toList(),
         ),
@@ -67,6 +113,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
-  
 }
